94c74c0d8880357fdb145561918a054d32196249
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 10:52:11 -0300

    adds pg condition to models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -9,6 +9,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+const pg = require('pg');
+
+if (!config.dialectModule) {
+  config.dialectModule = pg;
+}
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
