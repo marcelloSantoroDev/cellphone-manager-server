f3353d40632468bd4f9048cafc6853589e7b21e4
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 15:45:53 -0300

    fix: config.js

--- src/config/config.js
+++ src/config/config.js
@@ -10,10 +10,19 @@ module.exports = {
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
-  }
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+
 };
