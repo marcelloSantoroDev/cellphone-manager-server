2b5ae56d1505511407fee500533267cd48477ab8
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 12:09:42 -0300

    removes listen function from whole project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const app = require('./app');
 require('dotenv').config();
 
 
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
-const server = app.listen(PORT, async () => {
-    console.log(`rodando na porta ${PORT}`);
-});
+// const server = app.listen(PORT, async () => {
+//     console.log(`rodando na porta ${PORT}`);
+// });
 
 module.exports = server;
