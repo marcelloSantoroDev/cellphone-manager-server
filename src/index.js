2a8c5abac4fb9336f37a5b8b7ff289dd1c953476
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 14:48:42 -0300

    returns to adds vercel-build script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const app = require('./app');
 require('dotenv').config();
 
-module.exports = app;
+// module.exports = app;
 
-// const PORT = process.env.PORT;
+const PORT = process.env.PORT;
 
-// const server = app.listen(PORT, async () => {
-//     console.log(`rodando na porta ${PORT}`);
-// });
+const server = app.listen(PORT, async () => {
+    console.log(`rodando na porta ${PORT}`);
+});
 
-// module.exports = server;
+module.exports = server;
