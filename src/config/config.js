a6a6e7ae8b4db377a9d053a9c43ecc5742d8637d
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 14:31:48 -0300

    tries serverless postgres w/ new environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -2,18 +2,18 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DATABASE_USER || "postgres",
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    host: process.env.DATABASE_HOST,
     dialect: 'postgres',
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DATABASE,
+    host: process.env.DATABASE_HOST,
     dialect: 'postgres',
   }
 };
