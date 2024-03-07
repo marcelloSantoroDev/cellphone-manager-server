33be953d8b4a3185ae7c9d3de6ba5d6b0390a617
Marcello <marcelloaltermundi@gmail.com>
2024-03-07 10:58:29 -0300

    changes use_env_variable configuration in models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -17,10 +17,12 @@ if (!config.dialectModule) {
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  const databaseUrl = process.env[config.use_env_variable] || config.use_env_variable;
+  sequelize = new Sequelize(databaseUrl, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
 
 fs
   .readdirSync(__dirname)
